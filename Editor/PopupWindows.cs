92e1576100a37185c9b8835ff3869f8c874dcd39
Saberslay <dansteamshell@gmail.com>
2023-05-02 10:21:51 +0100

    remade the popup window

--- Editor/PopupWindows.cs
+++ Editor/PopupWindows.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 namespace Saberslay.SlayTools.editor.PopupWindows
 {
     public class PopupWindows : EditorWindow {
-        [MenuItem("Slayer Tools/Help/About")]
-        public static void About() {
-            GetWindow<PopupWindows>("About");
+        static bool showpop = true;
+        [MenuItem("Slayer Tools/Info")]
+        public static void Init() {
+                PopupWindows window = ScriptableObject.CreateInstance<PopupWindows>();
+                window.position = new Rect(Screen.width / 2, Screen.height / 2, 440, 150);
+                window.ShowPopup();
         }
         void OnGUI() {
+            
+            EditorGUILayout.LabelField("info", EditorStyles.boldLabel);
             EditorGUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            GUILayout.Label("If you like to support me in development in this tool");
+            EditorGUILayout.LabelField("If you like this tool feel free to", EditorStyles.wordWrappedLabel);
             HelperFunctions.Button.linkButton(110, 20, "Buy me a Coffee", "https://ko-fi.com/saberslay");
-            GUILayout.FlexibleSpace();
+            EditorGUILayout.LabelField("If you went to                    ", EditorStyles.wordWrappedLabel);
             EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("If you like to help me out in the development in this tool", EditorStyles.wordWrappedLabel);
+            HelperFunctions.Button.linkButton(110, 20, "Github", "https://github.com/saberslay/Slayer-Tools");
+            EditorGUILayout.EndHorizontal();
+            GUILayout.Space(5);
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Close!")) this.Close();
+            GUILayout.Space(5);
         }
     }
 }
